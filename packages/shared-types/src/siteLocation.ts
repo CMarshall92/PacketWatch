ed6aa77e0c69ce0dba8dab4c972e45d11017611a
Cmarshall92 <rookiegamedev@gmail.com>
2025-09-03 10:23:51 +0100

    chore: cleanup

--- packages/shared-types/src/siteLocation.ts
+++ packages/shared-types/src/siteLocation.ts
@@ -1,12 +1,12 @@
 export interface SiteLocation {
-    slug: string;
-    isApi: boolean;
-    href: string;
-    label: string;
-    icon: string;
-  }
-  
-  export interface SiteLocationResponse {
-    data: SiteLocation[]
-    prevSelectedSlug: string
-  }
+  slug: string;
+  isApi: boolean;
+  href: string;
+  label: string;
+  icon: string;
+}
+
+export interface SiteLocationResponse {
+  data: SiteLocation[]
+  prevSelectedSlug: string
+}
